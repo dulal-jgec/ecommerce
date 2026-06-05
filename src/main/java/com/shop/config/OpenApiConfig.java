87c050ebd114ef05d58ffe157c64cb6f0a55df0d
dulal-jgec <skdulalislam1@gmail.com>
2026-06-05 16:50:23 +0530

    Add autherization in config

--- src/main/java/com/shop/config/OpenApiConfig.java
+++ src/main/java/com/shop/config/OpenApiConfig.java
@@ -3,25 +3,38 @@ package com.shop.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfig {
-			
-	@Bean
-	public OpenAPI ecommerceOpenAPI() {
-		 
-		return new OpenAPI()
-				.info(new Info()
-					.title("Ecommerece Shop API")	
-					.version("1.0")
-					.description("Productioin-grade Ecommerce Backend API built with spring Boot")
-					.contact(new Contact()
-							.name("Sk Dulal Islam")
-							)
-						);
-				
-	}
+
+    @Bean
+    public OpenAPI ecommerceOpenAPI() {
+
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Ecommerce Shop API")
+                        .version("1.0")
+                        .description("Production-grade Ecommerce Backend API built with Spring Boot")
+                        .contact(new Contact()
+                                .name("SK Dulal Islam")))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ));
+    }
 }
